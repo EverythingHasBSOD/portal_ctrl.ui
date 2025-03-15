5ffff20c818aa6e08470f02482b68282ee8d06e9
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-03-15 23:26:34 +0400

    Upload All
    Upload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload AllUpload All.spam

---
+++ app.js
@@ -0,0 +1,12 @@
+// varibles
+let img = document.getElementById('image');
+
+// orange portal
+function orangeportal() {
+ img.src = './images/portalorange.gif'
+}
+
+// blue portal
+function blueportal() {
+ img.src = './images/portal.gif'
+}
